4f6cda6da3bc8fdbe2e41c09478b8805d092fb24
ivyugin <ilya.vyugin@gmail.com>
2021-03-28 11:28:26 +0300

    render template return string/

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactDOMServer from 'react-dom/server';
 import { BrowserRouter } from 'react-router-dom';
 import App from "./components/App/App.js";
 import './index.css';
@@ -7,19 +8,24 @@ import './index.css';
 import dataArr from './data/data.json';
 
 function renderTemplate(alias, data) {
-  return (
-    <App 
-      alias={alias}
-      data={data}
-    />
+  return ReactDOMServer.renderToString (
+    <BrowserRouter>
+      <App 
+        alias={alias}
+        data={data}
+      />
+    </BrowserRouter>
   );
 }
 
 window.renderTemplate = renderTemplate;
+//console.log(window)
 
 ReactDOM.render(
-  <BrowserRouter>
-    {renderTemplate(dataArr[0].alias, dataArr[0].data)}
-  </BrowserRouter>,
+    <BrowserRouter>
+      <div dangerouslySetInnerHTML={{
+        __html: renderTemplate(dataArr[0].alias, dataArr[0].data)
+      }}></div>
+    </BrowserRouter>,
   document.getElementById("root")
 );
